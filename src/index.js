c5c1e7df3d3d61fd0ccc66785fb0cdc592ef82c8
Faraz Ghani <farazghani@Mac.lan>
2025-12-05 23:32:49 +0530

    added deployed fe

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'test') {
 const app = express();
 
 app.use(cors({
-  origin: "taskmanager-l32aoje24-farazs-projects-5e7e4223.vercel.app", // or your Vercel domain
+  origin: "taskmanager-fe-six.vercel.app", // or your Vercel domain
   credentials: true,
 }));
 
